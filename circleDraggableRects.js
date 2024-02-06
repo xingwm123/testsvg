a377f2a55df41294d532f4f0c7323efecf48c216
xingwm123 <376860997@qq.com>
2024-02-06 12:18:58 +0800

    拖动的宽度的时候，更新线

--- circleDraggableRects.js
+++ circleDraggableRects.js
@@ -4,21 +4,17 @@ import { createRoundAndLine } from './createRoundAndLine.js'
 import { sharedState } from './common.js';
 
 
+let currentRect = null;
 
+let pathAssociations = {};
+
+const arrowLength = 10; // 箭头长度的估计值，根据实际标记大小调整
+
+let startHorizontalLength = 100; // 起点的水平长度
+let endHorizontalLength = 100; // 终点的水平长度
 
 export function createDraggableRects(svg, rects) {
 
-    let currentRect = null;
-
-    let pathAssociations = {};
-
-    const arrowLength = 10; // 箭头长度的估计值，根据实际标记大小调整
-
-    let startHorizontalLength = 100; // 起点的水平长度
-    let endHorizontalLength = 100; // 终点的水平长度
-
-
-    
 
     // 绘制矩形并应用拖动行为
     const rectSelection = svg.selectAll('rect')
@@ -64,18 +60,18 @@ export function createDraggableRects(svg, rects) {
         d3.select(this)
           .attr('x', d.x)
           // .attr('y', d.y); // 这行被注释掉或移除，因为我们不更新y坐标
-    
+
         // 更新关联圆形的位置
         let id = d.id.replace("rect-","");
         svg.selectAll(`.circle-right-${id}`)
           .attr('cx', d.x + d.width + 5) // 更新为矩形的右上角X坐标
           .attr('cy', d.y + d.height / 2); // Y坐标保持不变
-    
+
         // 更新当前拖动的矩形的位置
         d3.select(this)
           .attr('x', d.x = event.x)
           // .attr('y', d.y); // 同样，不更新y坐标
-    
+
         // 更新与当前矩形关联的活动矩形和拖拽控制点的位置
         svg.select(`#rect1-right-${id}`)
           .attr('x', d.x)
@@ -87,58 +83,62 @@ export function createDraggableRects(svg, rects) {
           }
         svg.select(`#polygon-right-${id}`)
           .attr("transform", "translate(" + (storedTriangleX) + ", " + (d.y + d.height) + ")");
-        
+
         d3.select(`#right-anchor-${id}`).attr("x", d.x + d.width - 3); // y坐标保持不变
         d3.select(`#left-anchor-${id}`).attr("x", d.x - 3); // 同上
-    
+
         updatePathPositions(id+"");
     }
 
-    function updatePathPositions(objectId) {
-      pathAssociations = sharedState.getPathAssociations();
-      // 根据被拖动对象的ID查找和更新所有关联的路径
-      Object.entries(pathAssociations).forEach(([pathId, association]) => {
-        if (association.start.id === objectId || association.end.id === objectId) {
-          // 如果当前路径与被拖动的对象相关联，重新计算并更新路径
-          let newPathD = calculatePathD(association.start, association.end);
-          d3.select('#' + pathId).attr('d', newPathD);
-        }
-      });
-    }
-  
-    function calculatePathD(startObject, endObject) {
-      // 根据起始和终点对象的位置计算路径的"d"属性
-      // 这里仅为示例，具体实现应根据起始和终点对象的实际位置来计算
-  
-      // const pathData = `M ${startX},${startY}
-      //                   C ${startX + startHorizontalLength},${startY} ${endX - endHorizontalLength},${endY} ${endX},${endY}`;
-      let startid = startObject.id;
-      let endid = endObject.id;
-      let start2 = d3.select("#circle-right-"+startid);
-      // let end2 = d3.select("#rect"+endid);
-      //startObject = {"id":startid,"x":start2.attr("x"),"y":start2.attr("y")}
-      //endObject = {"id":endid,"x":end2.attr("x"),"y":end2.attr("y")}
-  
-      let startX = parseInt(start2.attr('cx')),
-        startY = parseInt(start2.attr('cy'));
-  
-      let endRect = d3.select("#rect-"+endid);
-      const rect2X = parseInt(endRect.attr('x')),
-        rect2Y = parseInt(endRect.attr('y')),
-        rect2Height = parseInt(endRect.attr('height'));
-      let endX = rect2X - arrowLength; // 考虑箭头长度的调整
-      let endY = rect2Y + rect2Height / 2;
-  
-      let pathData = `M ${startX},${startY} C ${startX+ startHorizontalLength},${startY} ${endX - endHorizontalLength},${endY} ${endX},${endY}`;
-      return pathData;
-    }
+
+
+
 
 
     function dragEnded(event, d) {
         d3.select(this).classed('active', false);
-      
+
       }
 
 
 
+}
+
+export function calculatePathD(startObject, endObject) {
+  // 根据起始和终点对象的位置计算路径的"d"属性
+  // 这里仅为示例，具体实现应根据起始和终点对象的实际位置来计算
+
+  // const pathData = `M ${startX},${startY}
+  //                   C ${startX + startHorizontalLength},${startY} ${endX - endHorizontalLength},${endY} ${endX},${endY}`;
+  let startid = startObject.id;
+  let endid = endObject.id;
+  let start2 = d3.select("#circle-right-"+startid);
+  // let end2 = d3.select("#rect"+endid);
+  //startObject = {"id":startid,"x":start2.attr("x"),"y":start2.attr("y")}
+  //endObject = {"id":endid,"x":end2.attr("x"),"y":end2.attr("y")}
+
+  let startX = parseInt(start2.attr('cx')),
+    startY = parseInt(start2.attr('cy'));
+
+  let endRect = d3.select("#rect-"+endid);
+  const rect2X = parseInt(endRect.attr('x')),
+    rect2Y = parseInt(endRect.attr('y')),
+    rect2Height = parseInt(endRect.attr('height'));
+  let endX = rect2X - arrowLength; // 考虑箭头长度的调整
+  let endY = rect2Y + rect2Height / 2;
+
+  let pathData = `M ${startX},${startY} C ${startX+ startHorizontalLength},${startY} ${endX - endHorizontalLength},${endY} ${endX},${endY}`;
+  return pathData;
+}
+
+export function updatePathPositions(objectId) {
+  pathAssociations = sharedState.getPathAssociations();
+  // 根据被拖动对象的ID查找和更新所有关联的路径
+  Object.entries(pathAssociations).forEach(([pathId, association]) => {
+    if (association.start.id === objectId || association.end.id === objectId) {
+      // 如果当前路径与被拖动的对象相关联，重新计算并更新路径
+      let newPathD = calculatePathD(association.start, association.end);
+      d3.select('#' + pathId).attr('d', newPathD);
+    }
+  });
 }
--- createDeformationRectangle.js
+++ createDeformationRectangle.js
@@ -1,13 +1,13 @@
 
 
-
+import { updatePathPositions } from './circleDraggableRects.js'
 
 
 export function createDeformationRectangle(svg, rects) {
 
     rects.forEach(rect => {
         let id = rect.id.replace("rect-", "");
-    
+
         svg.append("rect")
             .attr("id", `right-anchor-${id}`)
             .attr("x", rect.x + rect.width - 3)
@@ -17,8 +17,8 @@ export function createDeformationRectangle(svg, rects) {
             .style("fill", "black")
             .style("cursor", "ew-resize")
             .call(d3.drag().on("drag", function(event) { dragRightAnchor(event, rect); }));
-    
-    
+
+
         svg.append("rect")
             .attr("id", `left-anchor-${id}`)
             .attr("x", rect.x - 3)
@@ -29,15 +29,16 @@ export function createDeformationRectangle(svg, rects) {
             .style("cursor", "ew-resize")
             .call(d3.drag().on("drag", function(event) { dragLeftAnchor(event, rect); }));
         });
-    
-    
+
+
         function dragRightAnchor(event, rect) {
             var newWidth = Math.max(10, event.x - rect.x);
             rect.width = newWidth;
             d3.select(`#rect-${rect.id}`).attr("width", newWidth);
             updateRelatedElements(rect);
+            updatePathPositions(rect.id.replace("rect-","")+"");
         }
-      
+
         function dragLeftAnchor(event, rect) {
             var newWidth = Math.max(10, rect.x + rect.width - event.x);
             var newX = rect.x + rect.width - newWidth;
@@ -47,9 +48,10 @@ export function createDeformationRectangle(svg, rects) {
                 .attr("x", newX)
                 .attr("width", newWidth);
             updateRelatedElements(rect);
+          updatePathPositions(rect.id.replace("rect-","")+"");
         }
-    
-    
+
+
         function updateRelatedElements(rect) {
             let id = rect.id.replace("rect-","")
               // 更新当前拖动的矩形的位置
@@ -60,7 +62,7 @@ export function createDeformationRectangle(svg, rects) {
             .attr('y', rect.y)
             .attr('width', rect.width)
             .attr('height', rect.height);
-            
+
             d3.selectAll(`.circle-right-${id}`)
               .attr('cx', rect.x + rect.width+8) // 更新为矩形的右上角X坐标
               .attr('cy', rect.y + rect.height / 2); // 更新为矩形的中点Y坐标
